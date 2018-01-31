951c30a456fb109c39b953f99f48b65c57f67465
Andreas Groll <andreas.groll@gmail.com>
2018-01-31 21:22:12 +0100

    codebuild environment is missing xargs

--- cfn/prepare_template.sh
+++ cfn/prepare_template.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -o errexit
-
+env
+sudo yum install findutils
 account_id="$(aws sts get-caller-identity --query Account --output text | xargs echo -n)"
 echo using account ${account_id} in ${AWS_REGION}
 sed -e "s/REGION/${AWS_REGION}/g" -e "s/ACCOUNT_ID/${account_id}/g" api-template.yml > api.yml
